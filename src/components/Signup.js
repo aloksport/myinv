d9ed90917aba5be8e9ae2b9dd9d5ddbceb20c2a6
aloksport <alok@sportainmentdesign.com>
2024-10-24 20:37:39 +0530

    Test from Home
    Test from Home

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,29 @@
-const Signup = () => {
-  return <h1>Signup</h1>;
-};
+import { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+function Signup() {
+  const [name, setName] = useState("");
 
-export default Signup;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`The name you entered was: ${name}`)
+  }
+
+  
+    
+}
+function AddToCart({productId}) {
+  async function addToCart(formData) {
+    'use server'
+    const productId = formData.get('productId')
+    await updateCart(productId)
+  }
+  return (
+    <form action={addToCart}>
+        <input type="hidden" name="productId" value={productId} />
+        <button type="submit">Add to Cart</button>
+    </form>
+
+  );
+}
+
+export default AddToCart;
